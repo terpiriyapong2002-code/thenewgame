7e429c9928113d84450b54d01489def8519406de
terpiriyapong2002-code <terpiriyapong2002@gmail.com>
2025-11-13 11:45:47 +0700

    Update game files2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/thenewgame/",
   plugins: [react()],
+
+  // REQUIRED for GitHub Pages
+  base: '/thenewgame/',
 })
